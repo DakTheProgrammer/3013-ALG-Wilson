4d618c8f7f97f9ce17453febf5deec8f2b35ec85
DakTheProgrammer <dtw3200@live.com>
2020-02-15 13:37:42 -0600

    Saving changes of comments and output. Ongoing!

--- Assignments/05-ProcessingInLinearTime(P-2)/ListDictionary.h
+++ Assignments/05-ProcessingInLinearTime(P-2)/ListDictionary.h
@@ -1,3 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Dakota Wilson
+// Email:            dtw3200@live.com
+// Label:            P-2
+// Title:            Assignment 5 - Heapify
+// Course:           3013
+// Semester:         Spring 2020
+//
+// Description:
+//      A linked list search program that stores a json file with a dictionary
+//  in it seperating the words and definition. With that information the program
+//  then allowes the user to type in a series of charecters. Everytime a user 
+//  enters a char the program will search through the list to find all the words
+//  with a substring of the chars entered and returns the top ten results plus 
+//  the time it took to search the list.
+//
+/////////////////////////////////////////////////////////////////////////////////
 #pragma once
 #include <string>
 #include <iostream>
@@ -5,24 +23,90 @@
 
 using namespace std;
 
+/**
+ * Node
+ * 
+ * Description:
+ *      A node that holds 2 strings for a dictionary 
+ * 		and a next pointer
+ * 
+ * Public Methods:
+ *      string 					definition
+ *		string 					Word
+ *		Node* 					next	
+ * 
+ * Private Methods:
+ *      None
+ * 
+ * Usage: 
+ * 		Node Temp;				//Creates node
+ *      
+ */
 struct Node
 {
-	string definition;
-	string Word;
-	Node* next;
+	string definition;			//holds def of word
+	string Word;				//holds the word
+	Node* next;					//pointer to a next node in the list
 };
 
+/**
+ * ListDictionary
+ * 
+ * Description:
+ *      A linked list that holds a dictionary and 
+ * 		can search for substrings of chars
+ * 
+ * Public Methods:
+ *      						ListDictionary() 
+ *      void 					insert(string W, string D)
+ *      void 					print()
+ *		void 					search(string input) 
+ * 		void 					printTenWords()
+ * 		int 					getTermsFound()
+ * 
+ * Private Methods:
+ *      Node* 					Head;
+ *		Node* 					Tail;	
+ *		int 					length;
+ *		string 					TenWords[10];
+ *		int 					TermsFound;
+ * 
+ * Usage: 
+ * 
+ *      ListDictionary LL; 		//creates list
+ * 		LL.insert("A", "B");	//adds a word A and a definition b to list
+ * 		LL.print();				//prints the list to a Checking.txt file
+ * 		LL.search("ste");		//searches list for words with substring ste
+ * 		ll.printTenWords();		//prints top ten results from searching
+ * 		ll.getTermsFound();		//returns the terms found from searching
+ *      
+ */
 class ListDictionary
 {
 private:
-	Node* Head;
-	Node* Tail;
-	int length;
-	string TenWords[10];
-	int TermsFound;
+	Node* Head;				//pointer to the head of the list
+	Node* Tail;				//pointer to the tail of the list
+	int length;				//length of the list
+	string TenWords[10];	//top ten words found when searching the list
+	int TermsFound;			//how many items found after searching list
 
 public:
 	
+	/**
+     * Public/Private/Protected : function_name
+     * 
+     * Description:
+     *      Describe the functions purpose
+     * 
+     * Params:
+     *      - list params
+     *      - one per line
+     *      - with return type
+     *      - and one line description
+     * 
+     * Returns:
+     *      - what does this function return (including the type)?
+     */
 	ListDictionary()
 	{
 		Head = nullptr;
@@ -35,6 +119,21 @@ public:
 		}
 	}
 
+	/**
+     * Public/Private/Protected : function_name
+     * 
+     * Description:
+     *      Describe the functions purpose
+     * 
+     * Params:
+     *      - list params
+     *      - one per line
+     *      - with return type
+     *      - and one line description
+     * 
+     * Returns:
+     *      - what does this function return (including the type)?
+     */
 	void insert(string W, string D)
 	{
 		Node* temp = new Node;
@@ -61,6 +160,21 @@ public:
 		temp = nullptr;
 	}
 
+	/**
+     * Public/Private/Protected : function_name
+     * 
+     * Description:
+     *      Describe the functions purpose
+     * 
+     * Params:
+     *      - list params
+     *      - one per line
+     *      - with return type
+     *      - and one line description
+     * 
+     * Returns:
+     *      - what does this function return (including the type)?
+     */
 	void print()
 	{
 		ofstream outfile;
@@ -75,6 +189,21 @@ public:
 		temp = nullptr;
 	}
 
+	/**
+     * Public/Private/Protected : function_name
+     * 
+     * Description:
+     *      Describe the functions purpose
+     * 
+     * Params:
+     *      - list params
+     *      - one per line
+     *      - with return type
+     *      - and one line description
+     * 
+     * Returns:
+     *      - what does this function return (including the type)?
+     */
 	void search(string input)
 	{
 		int timeSeen = 0;
@@ -103,6 +232,21 @@ public:
 		temp = nullptr;
 	}
 
+	/**
+     * Public/Private/Protected : function_name
+     * 
+     * Description:
+     *      Describe the functions purpose
+     * 
+     * Params:
+     *      - list params
+     *      - one per line
+     *      - with return type
+     *      - and one line description
+     * 
+     * Returns:
+     *      - what does this function return (including the type)?
+     */
 	void printTenWords()
 	{
 		bool printed = false;
@@ -126,6 +270,21 @@ public:
 		}
 	}
 
+	/**
+     * Public/Private/Protected : function_name
+     * 
+     * Description:
+     *      Describe the functions purpose
+     * 
+     * Params:
+     *      - list params
+     *      - one per line
+     *      - with return type
+     *      - and one line description
+     * 
+     * Returns:
+     *      - what does this function return (including the type)?
+     */
 	int getTermsFound()
 	{
 		return TermsFound;
--- Assignments/05-ProcessingInLinearTime(P-2)/Source.cpp
+++ Assignments/05-ProcessingInLinearTime(P-2)/Source.cpp
@@ -1,3 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Dakota Wilson
+// Email:            dtw3200@live.com
+// Label:            P-2
+// Title:            Assignment 5 - Heapify
+// Course:           3013
+// Semester:         Spring 2020
+//
+// Description:
+//      A linked list search program that stores a json file with a dictionary
+//  in it seperating the words and definition. With that information the program
+//  then allowes the user to type in a series of charecters. Everytime a user 
+//  enters a char the program will search through the list to find all the words
+//  with a substring of the chars entered and returns the top ten results plus 
+//  the time it took to search the list.
+//
+/////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include "JsonFacade.hpp"       
 #include <time.h>
@@ -12,42 +30,43 @@ using namespace std;
 int main()
 {
     JsonFacade J("dict_w_defs.json");
-    ListDictionary LList;
+    ListDictionary LList;                       //Linked list to hold dictionary
 
     char Typed;             
     string word = "";
 
-    vector<string> keys = J.getKeys();       //list of words in alpha order
+    vector<string> keys = J.getKeys();           //list of words in alpha order
 
     for(int i = 0; i < keys.size(); i++)
     {
         string DicWord = J.getKey(i);            //gets terms in alpha order 0-X
         string DicDef = J.getValue(DicWord);     //gets def of term
-        LList.insert(DicWord, DicDef);
+        LList.insert(DicWord, DicDef);           //loads deg and term
     }
 
     cout << "To exit program press capital Z\n\n\n";
 
     while ((Typed = getch()) != 'Z') 
     {
-        Timer T;
+        Timer T;                                  //makes timer
         T.Start();
-        word += Typed;
+        word += Typed;                            //string for the chars typed
 
         
 
         if((int)Typed != 10)
-        {   // if Typed is not a space print it
+        {                                         //if Typed is not a space print
             cout << word << "\n\n";
         }
-        LList.search(word);
+        LList.search(word);                       //searches list for substrings
 
         T.End();
-        double s = T.Seconds();
+        double s = T.Seconds();                   //stores time of search
 
-        cout << LList.getTermsFound() << " words found in: " << s << " Seconds\n\n";
+        cout << LList.getTermsFound() << " words found in: " << s 
+            << " Seconds\n\n";
 
-        LList.printTenWords();
+        LList.printTenWords();                    //prints top 10 results
 
         cout <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
     }
