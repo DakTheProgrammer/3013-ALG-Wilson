6e66b2bbb5ba0a52bc802547e7afeedcf5a3b743
DakTheProgrammer <dtw3200@live.com>
2020-04-05 14:32:01 -0500

    comments

--- Assignments/459/main.cpp
+++ Assignments/459/main.cpp
@@ -1,29 +1,98 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   
+// Author:           Dakota Wilson
+// Email:            dtw3200@live.com
+// Label:            459
+// Title:            459
+// Course:           CMPS-3013-201
+// Semester:         Spring 2020
+//
+// Description:
+//       A program that finds the total amount of connected graphs in a set
+//      
+// Usage:
+//         None
+//
+// Files:            
+//      main.cpp
+//		in.txt
+/////////////////////////////////////////////////////////////////////////////////
 #include <iostream> 
 #include <list> 
 #include <string>
 using namespace std;
 
+/**
+ * Graph
+ *
+ * Description:
+ *      list based graph
+ *
+ * Public Methods:
+ * 		DepFirSear(int Ver, bool marked[])
+ * 
+ * Public Methods:
+ *      Graph(int Ver)
+ * 		NewConnection(int Ver, int Con)
+ * 		TotalGraphs()
+ *
+ * Usage:
+ *      Graph g(6)									//makes graph of size 6
+ * 		
+ * 		g.NewConnection(1, 3)						//makes a conection for 
+ * 													//node 3 connected to 1
+ * 		
+ * 		g.TotalGraphs()								//outs the total graphs
+ *
+ */
 class Graph
 {
 	int Vertice, numTimes;
 
-	list<int>* GraphList;
+	list<int>* GraphList;								//list for nodes
 
+	/**
+	 * DepFirSear
+	 *
+	 * Description:
+	 *      does depth first search recursivly
+	 *
+	 * Params:
+	 *     - int ver
+	 * 	   - bool marked[]
+	 *
+	 * Returns:
+	 *     - NULL
+	 */
 	void DepFirSear(int Ver, bool marked[])
 	{
-		marked[Ver] = true;
+		marked[Ver] = true;								//marks the node
 
-		list<int>::iterator i;//used to make an iterator so searhing list is easier
+		list<int>::iterator i;							//used to make an 
+														//iterator so searhing
+														//list is easier
 
 		for (i = GraphList[Ver].begin(); i != GraphList[Ver].end(); i++)
 		{
 			if (!marked[*i])
 			{
-				DepFirSear(*i, marked);
+				DepFirSear(*i, marked);					//recursive call for bfs
 			}
 		}
 	}
 public:
+	/**
+	 * Graph
+	 *
+	 * Description:
+	 *      constructor that makes graph with size
+	 *
+	 * Params:
+	 *     - int ver
+	 * 	   
+	 * Returns:
+	 *     - NULL
+	 */
 	Graph(int Ver)
 	{
 		numTimes = 0;
@@ -33,6 +102,19 @@ public:
 		GraphList = new list<int>[Ver];
 	}
 
+	/**
+	 * Graph
+	 *
+	 * Description:
+	 *      makes a graph connection
+	 *
+	 * Params:
+	 *     - int Ver
+	 * 	   - int Con
+	 * 	   
+	 * Returns:
+	 *     - NULL
+	 */
 	void NewConnection(int Ver, int Con)
 	{
 		GraphList[Con].push_back(Ver);
@@ -40,13 +122,25 @@ public:
 		GraphList[Ver].push_back(Con);
 	}
 
+	/**
+	 * TotalGraphs
+	 *
+	 * Description:
+	 *      outs the total graphs
+	 *
+	 * Params:
+	 *     - none
+	 * 	   
+	 * Returns:
+	 *     - NULL
+	 */
 	void TotalGraphs()
 	{
 		bool* marked = new bool[Vertice];
 
 		for (int i = 0; i < Vertice; i++)
 		{
-			marked[i] = false;
+			marked[i] = false;						//loads markers
 		}
 
 		for (int i = 0; i < Vertice; i++)
@@ -55,7 +149,7 @@ public:
 			{
 				DepFirSear(i, marked);
 
-				numTimes++;
+				numTimes++;							//inrements num of graphs
 			}
 
 		}
@@ -72,7 +166,7 @@ int main()
 	bool checking;
 
 	getline(cin, Temps);
-	NumSet = stoi(Temps);
+	NumSet = stoi(Temps);							//string toint of numgraphs
 
 	getline(cin, Temps);
 
@@ -82,9 +176,9 @@ int main()
 
 		getline(cin, Temps);
 
-		GSize = int(Temps.at(0)) - 'A' + 1;
+		GSize = int(Temps.at(0)) - 'A' + 1;			//gets the max distance
 
-		Graph Gra(GSize);
+		Graph Gra(GSize);							//sets size of graph
 
 		i++;
 
@@ -94,18 +188,23 @@ int main()
 
 			if (Connections == "")
 			{
-				checking = false;
+				checking = false;					//breaks when there is a 
+													//blank
 			}
 			else
 			{
-				Gra.NewConnection(int(Connections.at(0)) - 'A', int(Connections.at(1)) - 'A');
+				Gra.NewConnection(
+					int(Connections.at(0)) - 'A',
+					int(Connections.at(1)) - 'A');
+													//makes connection
 			}
 		}
 		if (i != NumSet + 1 && i != 1)
 		{
-			cout << '\n';
+			cout << '\n';							//spacing for output
 		}
-		Gra.TotalGraphs();
+		Gra.TotalGraphs();							//prints total num of 
+													//graphs
 	}
 
 	return 0;
--- Assignments/484/main.cpp
+++ Assignments/484/main.cpp
@@ -1,3 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   
+// Author:           Dakota Wilson
+// Email:            dtw3200@live.com
+// Label:            484
+// Title:            484
+// Course:           CMPS-3013-201
+// Semester:         Spring 2020
+//
+// Description:
+//       takes in a set of numbers and shows them in order and how many of the
+//		same number are in the set.
+//      
+// Usage:
+//         None
+//
+// Files:            
+//      main.cpp
+//		in.txt
+/////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <vector>
 
@@ -15,7 +35,7 @@ int main()
 
 		for (int i = 0; i < Storage.size(); i++)
 		{
-			if (num == Storage[i].first)
+			if (num == Storage[i].first)						//if found break
 			{
 				temp = i;
 				found = true;
@@ -25,16 +45,16 @@ int main()
 		
 		if (Storage.empty() || !found)
 		{
-			Storage.push_back(make_pair(num, 1));
+			Storage.push_back(make_pair(num, 1));				//make a pair if empty or not there
 		}
 		else if(found)
 		{
-			Storage[temp].second = Storage[temp].second + 1;
+			Storage[temp].second = Storage[temp].second + 1; 	//if found increment
 		}
 		
 	}
 
-	for (int i = 0; i < Storage.size(); i++)
+	for (int i = 0; i < Storage.size(); i++)					//print results
 	{
 		cout << Storage[i].first << " " << Storage[i].second << '\n';
 	}
